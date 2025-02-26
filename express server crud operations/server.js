062009d6cc513b205ea90cc356fb3bb566eaff87
Khush Bakht Naveed <khush.naveed@dci-student.org>
2025-02-26 15:57:40 +0100

    endpoint add

--- express server crud operations/server.js
+++ express server crud operations/server.js
@@ -1,24 +1,61 @@
 import express from "express";
+import { v4 } from "uuid";
 
 const PORT = 4000;
 
 const app = express();
 
 const todoList = [
-  { id: 1, text: "take class", status: false },
-  { id: 2, text: "assignments", status: false },
-  { id: 3, text: "learning", status: false },
+  { id: v4(), text: "take class", status: false },
+  { id: v4(), text: "assignments", status: false },
+  { id: v4(), text: "learning", status: false },
 ];
 
+//middleware - parsing incoming json data
+app.use(express.json());
+
+const todoMap = new Map(todoList.map((item) => [item.id, item])); //O(1)
+
 //reading all tasks
+app.get("/alltasks", (req, res) => {
+  console.log(todoMap);
+  console.log(todoList);
+  res.send(todoList);
+});
 
-//reading 1 task
+app.get("/alltasks/:id", (req, res) => {
+  const task = todoMap.get(req.params.id);
+  res.json(task || { message: "Task not found" });
+});
 
-//create task
+app.post("/alltasks", (req, res) => {
+  const task = { id: v4(), text: req.body.text };
+  todoList.push(task);
+  res.send(todoList);
+});
 
-//update task
+//update task -- chnages in exsisiting resource
+app.patch("/alltasks/:id", (req, res) => {
+  const task = todoMap.get(req.params.id);
+  const { text, status } = req.body;
+  task.text = text;
+  task.status = status;
+  todoMap.set(task.id, task);
+  res.json(task || { message: "Task not found" });
+});
+
+//put request - replace the whole item
 
 //delete task
+app.delete("/alltasks/:id", (req, res) => {
+  const taskId = req.params.id;
+  todoMap.delete(taskId);
+  const index = todoList.findIndex((item) => item.id === req.params.id);
+  todoList.splice(index, 1);
+  console.log(todoMap);
+  console.log(todoList);
+  res.json({ message: "Task deleted successfully" });
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on port: ", PORT);
